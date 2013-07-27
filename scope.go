eabc053903c376af54002c6152849a69ae7a8ea4
E.Azer Koçulu <azer@kodfabrik.com>
2013-07-27 01:17:38 -0700

    add default enabling

--- scope.go
+++ scope.go
@@ -2,6 +2,7 @@ package debug
 
 type DebugScopeType func (scope string) func (text string, args ...interface{})
 
-func DebugScope (scope string) func (text string, args ...interface{}) {
+func DebugScope (scope string, keys ...string) func (text string, args ...interface{}) {
+	enabled = append(enabled, keys...)
 	return Debug
 }
