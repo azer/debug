3e8dcc6be92125fc171b772530b79e6ca7a7f7c9
E.Azer Koçulu <azer@kodfabrik.com>
2013-07-27 04:55:59 -0700

    add default keys only if DEBUG isnt specified

--- scope.go
+++ scope.go
@@ -1,8 +1,14 @@
 package debug
 
+import "os"
+
 type DebugScopeType func (scope string) func (text string, args ...interface{})
 
 func DebugScope (scope string, keys ...string) func (text string, args ...interface{}) {
-	enabled = append(enabled, keys...)
+	if os.Getenv("DEBUG") == "" {
+		enabled = append(enabled, keys...)
+		return Debug
+	}
+
 	return Debug
 }
